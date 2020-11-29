b42d051afdd0034f027e2c14e4440af63aefb95b
khorevigor <dostup.96@gmail.com>
2020-11-29 13:45:43 +0300

    Create Marvel Avengers: End game info screen (#1)
    * Create Marvel Avengers: End game info screen
    
    Created movie details xml-layout

--- app/src/main/java/com/dsphoenix/aacscratch/MainActivity.kt
+++ app/src/main/java/com/dsphoenix/aacscratch/MainActivity.kt
@@ -2,10 +2,12 @@ package com.dsphoenix.aacscratch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        this.window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
         setContentView(R.layout.activity_main)
     }
 }
