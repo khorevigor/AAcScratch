6aaa0f7a9603a7289c30946e0b4895cf27f605e7
Ihar Khorau <dostup.96@gmail.com>
2020-12-17 21:46:11 +0300

    Remove duplicates

--- app/src/main/java/com/dsphoenix/aacscratch/MovieListFragment.kt
+++ app/src/main/java/com/dsphoenix/aacscratch/MovieListFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.dsphoenix.aacscratch.movieslist.MovieListFragmentDirections
 
 class MovieListFragment : Fragment() {
 
@@ -14,7 +15,7 @@ class MovieListFragment : Fragment() {
     {
         val view = inflater.inflate(R.layout.fragment_movies_list, container, false)
 
-        view.findViewById<ImageView>(R.id.movie_preview_tile).setOnClickListener {
+        view.findViewById<ImageView>(R.id.rv_movies_list).setOnClickListener {
             val action = MovieListFragmentDirections.actionMovieListFragmentToMovieDetailsFragment3()
             findNavController().navigate(action)
         }
